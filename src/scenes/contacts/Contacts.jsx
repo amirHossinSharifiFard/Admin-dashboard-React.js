fb6580ff39c78d8a761704e9373a3bb5e245bd87
Amir Hossin Sharifi Fard <amirawade7@gmail.com>
2023-11-24 09:38:19 +0330

    build Contacts components with GridTools

---
+++ src/scenes/contacts/Contacts.jsx
@@ -0,0 +1,111 @@
+import React from "react";
+import { Box, useTheme } from "@mui/material";
+import { tokens } from "../../theme";
+import { DataGrid, GridToolbar } from "@mui/x-data-grid";
+
+//  Data
+import { mockDataContacts } from "../../data/mockData";
+
+// Icons
+import AdminPanelSettingsOutlinedIcon from "@mui/icons-material/AdminPanelSettingsOutlined";
+import LockOpenOutlinedIcon from "@mui/icons-material/LockOpenOutlined";
+import SecurityOutlinedIcon from "@mui/icons-material/SecurityOutlined";
+
+// components
+import Header from "../../components/Header";
+
+const Team = () => {
+  const theme = useTheme();
+  const colors = tokens(theme.palette.mode);
+
+  const columns = [
+    {
+      field: "id",
+      headerName: "ID",
+      flex: 0.5
+    },
+    {
+      field: "registrarId",
+      headerName: "RegistrarId"
+    },
+    {
+      field: "name",
+      headerName: "Name",
+      flex: 1,
+      cellClassName: "name-columns--cell"
+    },
+    {
+      field: "age",
+      headerName: "Age",
+      typ: "number",
+      headerAlign: "left",
+      align: "left"
+    },
+    {
+      field: "email",
+      headerName: "Email",
+      flex: 1
+    },
+    {
+      field: "phone",
+      headerName: "Phone",
+      flex: 1
+    },
+    {
+      field: "address",
+      headerName: "Address",
+      flex: 1
+    },
+    {
+      field: "city",
+      headerName: "City",
+      flex: 1
+    },
+    {
+      field: "zipCode",
+      headerName: "Zipcode",
+      flex: 1
+    }
+  ];
+  return (
+    <Box m='20px' width='100'>
+      <Header title='CONTACTS' subTitle='List Contacts for future Refrence' />
+      <Box
+        m='40px 0 0 0'
+        height='75vh'
+        sx={{
+          "& .MuiDataGrid-root": {
+            border: "none"
+          },
+          "& .MuiDataGrid-cell": {
+            borderBottom: "none"
+          },
+          "& .name-columns--cell": {
+            color: colors.greenAccent[300]
+          },
+          "& .MuiDataGrid-columnHeaders": {
+            backgroundColor: colors.blueAccent[700],
+            borderBottom: "none"
+          },
+          "& .MuiDataGrid-virtualScroller": {
+            backgroundColor: colors.primary[400]
+          },
+          "& .MuiDataGrid-footerContainer": {
+            borderTop: "none",
+            backgroundColor: colors.blueAccent[700]
+          },
+          "& .MuiDataGrid-toolbarContainer .MuiButton-text": {
+            color: `${colors.grey[100]} !important`
+          }
+        }}>
+        <DataGrid
+          rows={mockDataContacts}
+          columns={columns}
+          components={{ toolbar: GridToolbar }}
+        />
+      </Box>
+    </Box>
+  );
+};
+
+export default Team;
--- src/scenes/global/SideBar.jsx
+++ src/scenes/global/SideBar.jsx
@@ -87,7 +87,32 @@ const SideBar = () => {
             )}
           </MenuItem>
 
-          
+          {!isCollapsed && (
+            <Box mb='25px'>
+              <Box display='flex' justifyContent='center' alignItems='center'>
+                <img
+                  alt='profile-user'
+                  width='100px'
+                  height='100px'
+                  src={`../../assets/user.png`}
+                  style={{ cursor: "pointer", borderRadius: "50%" }}
+                />
+              </Box>
+              <Box textAlign='center'>
+                <Typography
+                  variant='h2'
+                  color={colors.grey[100]}
+                  fontWeight='bold'
+                  sx={{ m: "10px 0 0 0" }}>
+                  AmirHossein SharifiFard
+                </Typography>
+                <Typography variant='h5' color={colors.greenAccent[500]}>
+                  Front-End Developer
+                </Typography>
+              </Box>
+            </Box>
+          )}
+
           {/* Menu Items */}
           <Box paddingLeft={isCollapsed ? undefined : "10%"}>
             <Item
