61f14fb76178ddcdfff46bf40459f61d67cc3fc2
Amir Hossin Sharifi Fard <amirawade7@gmail.com>
2023-11-27 18:41:37 +0330

    creat pie chart and add to app

--- src/App.jsx
+++ src/App.jsx
@@ -14,8 +14,8 @@ import Form from "./scenes/form/Form";
 import Calendar from "./scenes/calendar/Calendar";
 import FAQ from "./scenes/faq/FAQ";
 import Bar from "./scenes/bar/Bar";
+import Pie from "./scenes/pie/Pie";
 // import Line from "./scenes/line";
-// import Pie from "./scenes/pie";
 // import Geography from "./scenes/geography";
 
 const App = () => {
@@ -37,7 +37,7 @@ const App = () => {
               <Route path='/calendar' element={<Calendar />} />
               <Route path='/faq' element={<FAQ />} />
               <Route path='/bar' element={<Bar />} />
-              {/* <Route path='/pie' element={<Pie />} /> */}
+              <Route path='/pie' element={<Pie />} />
               {/* <Route path='/line' element={<Line />} /> */}
               {/* <Route path='/geography' element={<Geography />} /> */}
             </Routes>
