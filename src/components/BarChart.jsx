d088bfb4fc877c4ccd798765cd289899451f4299
Amir Hossin Sharifi Fard <amirawade7@gmail.com>
2023-11-27 17:14:19 +0330

    prettier code

--- src/components/BarChart.jsx
+++ src/components/BarChart.jsx
@@ -1,133 +1,145 @@
 import React from "react";
 import { useTheme } from "@emotion/react";
 import { tokens } from "../theme";
-import { ResponsiveBar  } from "@nivo/bar";
+import { ResponsiveBar } from "@nivo/bar";
 
 // inja mockBarData ro be name data import mikonim
 import { mockBarData as data } from "../data/mockData";
 import { colors } from "@mui/material";
 
 const BarChart = () => {
-  const theme=useTheme()
-  const colors=tokens(theme.palette.mode)
-  return <ResponsiveBar
-    data={data}
-    keys={[
-        'hot dog',
-        'burger',
-        'sandwich',
-        'kebab',
-        'fries',
-        'donut'
-    ]}
-    indexBy="country"
-    margin={{ top: 50, right: 130, bottom: 50, left: 60 }}
-    padding={0.3}
-    valueScale={{ type: 'linear' }}
-    indexScale={{ type: 'band', round: true }}
-    colors={{ scheme: 'nivo' }}
-    defs={[
+  const theme = useTheme();
+  const colors = tokens(theme.palettemide);
+  return (
+    <ResponsiveBar
+      data={data}
+      theme={{
+        // add
+        axis: {
+          domain: {
+            line: {
+              stork: colors.grey[100]
+            }
+          }
+        },
+        legend: {
+          text: {
+            fill: colors.grey[100]
+          }
+        },
+        ticks: {
+          line: {
+            storke: colors.grey[100],
+            storkeWidth: 1
+          },
+          text: {
+            fill: colors.grey[100]
+          }
+        }
+      }}
+      keys={["hot dog", "burger", "sandwich", "kebab", "fries", "donut"]}
+      indexBy='country'
+      margin={{ top: 50, right: 130, bottom: 50, left: 60 }}
+      padding={0.5}
+      valueScale={{ type: "linear" }}
+      indexScale={{ type: "band", round: true }}
+      colors={{ scheme: "nivo" }}
+      defs={[
         {
-            id: 'dots',
-            type: 'patternDots',
-            background: 'inherit',
-            color: '#38bcb2',
-            size: 4,
-            padding: 1,
-            stagger: true
+          id: "dots",
+          type: "patternDots",
+          background: "inherit",
+          color: "#38bcb2",
+          size: 4,
+          padding: 1,
+          stagger: true
         },
         {
-            id: 'lines',
-            type: 'patternLines',
-            background: 'inherit',
-            color: '#eed312',
-            rotation: -45,
-            lineWidth: 6,
-            spacing: 10
+          id: "lines",
+          type: "patternLines",
+          background: "inherit",
+          color: "#eed312",
+          rotation: -45,
+          lineWidth: 6,
+          spacing: 10
         }
-    ]}
-    fill={[
+      ]}
+      fill={[
         {
-            match: {
-                id: 'fries'
-            },
-            id: 'dots'
+          match: {
+            id: "fries"
+          },
+          id: "dots"
         },
         {
-            match: {
-                id: 'sandwich'
-            },
-            id: 'lines'
+          match: {
+            id: "sandwich"
+          },
+          id: "lines"
         }
-    ]}
-    borderColor={{
-        from: 'color',
-        modifiers: [
-            [
-                'darker',
-                1.6
-            ]
-        ]
-    }}
-    axisTop={null}
-    axisRight={null}
-    axisBottom={{
+      ]}
+      borderColor={{
+        from: "color",
+        modifiers: [["darker", 1.6]]
+      }}
+      axisTop={null}
+      axisRight={null}
+      axisBottom={{
         tickSize: 5,
         tickPadding: 5,
         tickRotation: 0,
-        legend: 'country',
-        legendPosition: 'middle',
+        legend: "country",
+        legendPosition: "middle",
         legendOffset: 32,
         truncateTickAt: 0
-    }}
-    axisLeft={{
+      }}
+      axisLeft={{
         tickSize: 5,
         tickPadding: 5,
         tickRotation: 0,
-        legend: 'food',
-        legendPosition: 'middle',
+        legend: "food",
+        legendPosition: "middle",
         legendOffset: -40,
         truncateTickAt: 0
-    }}
-    labelSkipWidth={12}
-    labelSkipHeight={12}
-    labelTextColor={{
-        from: 'color',
-        modifiers: [
-            [
-                'darker',
-                1.6
-            ]
-        ]
-    }}
-    legends={[
+      }}
+      enableLabel={false}
+      labelSkipWidth={13}
+      labelSkipHeight={12}
+      labelTextColor={{
+        from: "color",
+        modifiers: [["darker", 1.6]]
+      }}
+      legends={[
         {
-            dataFrom: 'keys',
-            anchor: 'bottom-right',
-            direction: 'column',
-            justify: false,
-            translateX: 120,
-            translateY: 0,
-            itemsSpacing: 2,
-            itemWidth: 100,
-            itemHeight: 20,
-            itemDirection: 'left-to-right',
-            itemOpacity: 0.85,
-            symbolSize: 20,
-            effects: [
-                {
-                    on: 'hover',
-                    style: {
-                        itemOpacity: 1
-                    }
-                }
-            ]
+          dataFrom: "keys",
+          anchor: "bottom-right",
+          direction: "column",
+          justify: false,
+          translateX: 120,
+          translateY: 0,
+          itemsSpacing: 2,
+          itemWidth: 100,
+          itemHeight: 20,
+          itemDirection: "left-to-right",
+          itemOpacity: 0.85,
+          symbolSize: 20,
+          effects: [
+            {
+              on: "hover",
+              style: {
+                itemOpacity: 1
+              }
+            }
+          ]
         }
-    ]}
-    role="application"
-    ariaLabel="Nivo bar chart demo"
-    barAriaLabel={e=>e.id+": "+e.formattedValue+" in country: "+e.indexValue}
-/>
+      ]}
+      role='application'
+      ariaLabel='Nivo bar chart demo'
+      barAriaLabel={(e) =>
+        e.id + ": " + e.formattedValue + " in country: " + e.indexValue
+      }
+    />
+  );
 };
 
 export default BarChart;
