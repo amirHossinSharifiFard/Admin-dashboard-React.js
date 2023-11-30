d45dfa2ea6e53d0be6c1771ee2c43a59e24aca4d
Amir Hossin Sharifi Fard <amirawade7@gmail.com>
2023-11-30 13:40:46 +0330

    creat progressBar and StatBox

--- src/scenes/dashboard/DashBoard.jsx
+++ src/scenes/dashboard/DashBoard.jsx
@@ -1,19 +1,34 @@
 import React from "react";
-
 import Header from "../../components/Header";
-import { Box } from "@mui/material";
-// import { SpaceBar } from "@mui/icons-material";
+import { tokens } from "../../theme";
+import { Box, Button, IconButton, Typography, useTheme } from "@mui/material";
+import { mockTransactions } from "../../data/mockData";
+import {
+  DownloadOutlined,
+  Email,
+  PointOfSale,
+  PersonAdd,
+  Traffic
+} from "@mui/icons-material";
+import LineChart from "../../components/LineChart";
+import BarChart from "../../components/Barhart";
+import GeograpyChart from "../../components/GeograpyChart";
+import StatBox from "../../components/StatBox";
+import ProgressCircle from "../../components/ProgressCircle";
+
 import BarChart from "../../components/BarChart";
 const DashBoard = () => {
+  const theme = useTheme();
+  const colors = tokens(theme.palette.mode);
+  
   return (
     <Box m='20px'>
       <Box display='flex' justifyContent='space-between' alignItems='center'>
         <Header title='DASHBOARD' subTitle='Welcome to your Dashboard' />
-        
       </Box>
-     <Box height="75vh">
-      <BarChart />
-     </Box>
+    <Box>
+      
+    </Box>
     </Box>
   );
 };
